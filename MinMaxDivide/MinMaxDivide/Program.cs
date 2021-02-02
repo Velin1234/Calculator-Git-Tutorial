852a63593eb07f8ee3d0b20f9afc68259b47f63d
unknown <deathtalker19@abv.bg>
2021-02-02 11:27:23 +0200

    Final product

--- MinMaxDivide/MinMaxDivide/Program.cs
+++ MinMaxDivide/MinMaxDivide/Program.cs
@@ -25,11 +25,28 @@ namespace MinMaxDivide
             {
                 return Devide(num1, num2);
             }
+            if (num2 == 0)
+            {
+                Console.WriteLine("Cant divide by 0!");
+            }
         }
 
         private static decimal Devide(decimal num1, decimal num2)
         {
             return num1 / num2;
+        }
+        private static decimal MinMax(decimal num1, decimal num2)
+        {
+            if (num1 < num2)
+            {
+                return Console.WriteLine("Max is = "num2);
+                Console.WriteLine("Min is = "num1);
+            }
+            else
+            {
+                return Console.WriteLine("Max is = "num1 );
+                Console.WriteLine("Min is = "num2);
+            }
         }
     }
 }
